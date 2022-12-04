a0bce81cd5107ad430206fa41b6c179340430ab0
zyz <88691964+1012081978@users.noreply.github.com>
2022-12-05 00:49:31 +0800

    Update GitTest.java

--- test/GitTest.java
+++ test/GitTest.java
@@ -2,7 +2,7 @@ package test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello pull");
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git5");
         System.out.println("hot fix");
         System.out.println("github push");
+        System.out.println("hello pull");
     }
 }
